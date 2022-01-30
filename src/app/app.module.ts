2359f8ca5cf79538d25d8cc7fa4324104e9ab10a
Marc van Zyl <marcfvanzyl@gmail.com>
2022-01-29 23:31:52 -0500

    Push to pi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MeasurementSpo2Component } from './measurement-spo2/measurement-spo2.component';
 import {MatCardModule} from '@angular/material/card';
+import { MeasurementBpComponent } from './measurement-bp/measurement-bp.component';
+import { MeasurementTempComponent } from './measurement-temp/measurement-temp.component';
+import { MeasurementScaleComponent } from './measurement-scale/measurement-scale.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeasurementSpo2Component
+    MeasurementSpo2Component,
+    MeasurementBpComponent,
+    MeasurementTempComponent,
+    MeasurementScaleComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +26,8 @@ import {MatCardModule} from '@angular/material/card';
     MatToolbarModule,
     MatIconModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
